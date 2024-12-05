b263ab3ae1150858e83c6be3df3bb7522b3f2ad3
Iris Kim <blueiris1313@gmail.com>
2024-12-05 14:10:07 -0800

    Initial commit

---
+++ GitHubFollowersTests/GitHubFollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubFollowersTests.swift
+//  GitHubFollowersTests
+//
+//  Created by Iris Kim on 12/5/24.
+//
+
+import Testing
+@testable import GitHubFollowers
+
+struct GitHubFollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

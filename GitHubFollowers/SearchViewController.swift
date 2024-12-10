7ec16e0ae194aea1e9a7ef9982dd02276d124f92
blueiris13 <blueiris1313@gmail.com>
2024-12-10 00:38:09 -0800

    Added a custom alert

--- GitHubFollowers/SearchViewController.swift
+++ GitHubFollowers/SearchViewController.swift
@@ -37,7 +37,7 @@ class SearchViewController: UIViewController {
     @objc func pushFollowerListVC() {
         
         guard isUserNameEntered else {
-            print("No username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for.", buttonTitle: "Ok")
             return
         }
         
